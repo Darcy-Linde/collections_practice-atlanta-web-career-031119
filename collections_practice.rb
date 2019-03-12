c61807f70bcd894c2e367261294053ac94996a6d
Darcy Linde <lindedarcy@gmail.com>
2019-03-12 13:24:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort { |x,y| y <=> x }
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+  
+end
+
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker
+  
+end
+
+def find_a
+  
+end
+
+def sum_array
+  
+end
+
+def add_s
+  
+end
